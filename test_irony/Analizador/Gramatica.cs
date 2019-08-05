a4ed053535a180aeeb39ede89405548c31721cb4
Kvn1004 <pex089@gmail.com>
2019-08-04 20:04:37 -0600

    primer commit

---
+++ test_irony/Analizador/Gramatica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Ast;
+using Irony.Parsing;
+
+namespace test_irony.Analizador
+{
+    class Gramatica : Grammar
+    {
+        public Gramatica() : base(true)
+        {
+
+        }
+
+    }
+}
