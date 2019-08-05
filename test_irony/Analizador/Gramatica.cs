d90fdc12795d7b2fabace04fb8d4ca3c8a1bf458
Kvn1004 <pex089@gmail.com>
2019-08-04 23:35:19 -0600

    segundo commit

--- test_irony/Analizador/Gramatica.cs
+++ test_irony/Analizador/Gramatica.cs
@@ -12,7 +12,42 @@ namespace test_irony.Analizador
     {
         public Gramatica() : base(true)
         {
+            var NUMERO = new NumberLiteral("Numero");
+            var REVALUAR = ToTerm("Evaluar");
+            var PTCOMA = ToTerm(";");
+            var PARIZQ = ToTerm("(");
+            var PARDER = ToTerm(")");
+            var CORIZQ = ToTerm("[");
+            var CORDER = ToTerm("]");
+            var MAS = ToTerm("+");
+            var MENOS = ToTerm("-");
+            var POR = ToTerm("*");
+            var DIVIDIDO = ToTerm("/");
 
+            NonTerminal ini = new NonTerminal("ini");
+            NonTerminal instruccion = new NonTerminal("instruccion");
+            NonTerminal instrucciones = new NonTerminal("instrucciones");
+            NonTerminal expresion = new NonTerminal("expresion");
+
+            ini.Rule = instrucciones;
+
+            instrucciones.Rule = instruccion + instrucciones
+                | instruccion;
+
+            instruccion.Rule = REVALUAR + CORIZQ + expresion + CORDER + PTCOMA;
+
+            expresion.Rule = MENOS + expresion
+                | expresion + MAS + expresion
+                | expresion + MENOS + expresion
+                | expresion + POR + expresion
+                | expresion + DIVIDIDO + expresion
+                | NUMERO
+                | PARIZQ + expresion + PARDER;
+
+            RegisterOperators(1, MAS, MENOS);
+            RegisterOperators(2, POR, DIVIDIDO);
+
+            this.Root = ini;
         }
 
     }
