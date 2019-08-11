46d218b413b3e12163078983a12721256942d7a0
Kvn1004 <pex089@gmail.com>
2019-08-11 10:34:08 -0600

    commit final

--- test_irony/MainWindow.xaml.cs
+++ test_irony/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoIronyCS.sol.com.analizador;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,13 @@ namespace test_irony
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string entrada = txtEntrada.Text;
+            Sintactico sin = new Sintactico();
+            sin.analizar(entrada);
         }
     }
 }
